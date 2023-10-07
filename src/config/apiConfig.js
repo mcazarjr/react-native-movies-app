ec1abc48a8f92b7455200e59081b97e47201a1b3
mcazarjr <meraldocazar@gmail.com>
2023-10-07 10:20:38 -0700

    chore: include apiConfig file and edit gitignore to include it

---
+++ src/config/apiConfig.js
@@ -0,0 +1,6 @@
+export const API_BEARER_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWI1ZWMxNWJlNzYyYmNjOTU2MDE5MDJlMTc3Y2RjNCIsInN1YiI6IjYzOGQzNmJhYzNhYTNmMDBjODljZGJiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0Q6hWjVeX1kZdkgt7IPYa7s62nVyNTZuul7DIlQIHGI";
+export const API_URL = "https://api.themoviedb.org/3";
+export const API_IMG_URL_w185 = "https://image.tmdb.org/t/p/w185";
+export const API_IMG_URL_w342 = "https://image.tmdb.org/t/p/w185";
+export const API_LANG = "en-US";
